db6e0c5697ff70f7e23053aa98e645cd071f6528
SOROKKIM <115780040+SOROKKIM@users.noreply.github.com>
2023-12-18 11:21:33 +0900

    [level unrated] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.py
@@ -1,11 +1,24 @@
-def solution(array, n):
-    answer = 0
-    num = []
-    cnt = 0
-    for i in range(len(array)):
-        array.sort()
-        distance = abs(n-array[i])
-        num.append(distance)
-        cnt = array[num.index(min(num))]
+# def solution(array, n):
+#     answer = 0
+#     num = []
+#     cnt = 0
+#     for i in range(len(array)):
+#         array.sort()
+#         distance = abs(n-array[i])
+#         num.append(distance)
+#         cnt = array[num.index(min(num))]
     
-    return cnt
+#     return cnt
+
+
+def solution(array, n):
+    t_list = [ ]
+    array.sort()
+    for i in range (len(array)):
+        if n >= array[i]:
+            t_list.append(n-array[i])
+        elif n < array[i]:
+            t_list.append(array[i]-n)
+    # t_list.sort()
+    x = t_list.index(min(t_list))
+    return array[x]
