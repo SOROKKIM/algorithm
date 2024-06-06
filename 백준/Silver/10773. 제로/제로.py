d37752364f89f3229a8dfda039c6a08f41399481
SOROKKIM <115780040+SOROKKIM@users.noreply.github.com>
2024-06-07 00:10:20 +0900

    [Silver IV] Title: 제로, Time: 84 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,11 @@
-K = int(input())
+import sys
+
+K = int(sys.stdin.readline())
 stack = []
 sum = 0
 
 for i in range (K):
-    N = int(input())
+    N = int(sys.stdin.readline())
     if(N == 0):
         if(stack):
             sum -= stack.pop()
